a2fc4de472d8f2acceede8260c745a3decdad589
Gerard Almenara <ger.almenara@gmail.com>
2022-11-28 09:38:22 +0100

    Added accuracy and methods associated with it WIP: Most _most_played

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,24 +3,7 @@ class PagesController < ApplicationController
     @mmr = Valorant::MMRStats.mmr_data(params[:name], params[:tag])
     @account = Valorant::AccountStats.account_data(params[:name], params[:tag])
     @matches = Valorant::MatchStats.matches_history(params[:name], params[:tag])
-    @leg_shots, @head_shots, @body_shots = stats_from_matches(@matches)
-  end
-
-  private
-
-  def stats_from_matches(_matches)
-    leg_shots = 0
-    head_shots = 0
-    body_shots = 0
-    @matches.matches.each do |match|
-      match.all_players.each do |player|
-        next unless player.name == params[:name] && player.tag == params[:tag]
-
-        leg_shots += player.legshots
-        head_shots += player.headshots
-        body_shots += player.bodyshots
-      end
-    end
-    return leg_shots, head_shots, body_shots
+    @head_shots, @body_shots, @leg_shots = @matches.accuracy(params[:name], params[:tag])
+    @total_shots = @head_shots + @body_shots + @leg_shots
   end
 end
